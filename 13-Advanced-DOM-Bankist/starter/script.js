5861342af455ad01c552e54a77a8fdecca905533
mahmoud fouad <mfapolice@gmail.com>
2022-07-20 21:39:02 +0200

    update

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -86,7 +86,12 @@ const section1 = document.querySelector('#section--1');
 
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
-  // console.log(e.target.getBoundingClientRect());
-  console.log('current scroll (x,y)');
+  // console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+  console.log('current scroll (x,y)', window.pageXOffset, window.pageYOffset);
+  console.log(
+    'height/width view port',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
 });
