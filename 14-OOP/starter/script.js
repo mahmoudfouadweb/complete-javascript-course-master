3b3a234fe4f4c91695f5a5303f47f31c73250f24
Mahmoud Fouad Ali Abo Ali <84210982+mahmoudfouadweb@users.noreply.github.com>
2022-09-22 14:54:25 +0200

    get and set

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -95,16 +95,73 @@ class PersonCl {
   greatGreet() {
     console.log(`very welcome ${this.firstName}`);
   }
+
+  set firstName(name) {
+    if (name.includes(' ')) {
+      this._firstName = name;
+    } else {
+      console.log('Error');
+    }
+  }
+
+  get firstName() {
+    return this._firstName;
+  }
+
+  get age() {
+    return 2030 - this.birthYear;
+  }
 }
 
 const rafal = new PersonCl('Rafal', 2015);
+const yossef = new PersonCl('Yossef',2018)
+
+console.log('__________ YOSSEF _______________');
+console.log(yossef);
+yossef.firstName = 'Yossef Mahmoud'
+console.log(yossef);
+
+console.log(rafal);
+rafal.firstName = 'Rafal mahmoud';
+rafal.calcAge();
+
+console.log(rafal.firstName);
 
 console.log(rafal.__proto__ === PersonCl.prototype);
-console.dir(console);
 
 PersonCl.prototype.greet = function () {
   console.log(`Hey ${this.firstName}`);
 };
 
 rafal.greet();
-rafal.greatGreet()
+rafal.greatGreet();
+
+///////////////////////////////////////////////////////////////////////////
+
+const account = {
+  name: 'Mahmoud',
+  fullName: 'Mahmoud Fouad',
+  birthYear: 1991,
+  movement: [200, 312, 400, 50],
+
+  get calcAge() {
+    console.log(2030 - this.birthYear);
+  },
+
+  set calcAge(year) {
+    console.log(year - this.birthYear);
+  },
+
+  set fullName(name) {
+    if (name.includes(' ')) this.fullName = name;
+    else {
+      console.log('please check your correct full name');
+    }
+  },
+};
+
+account.calcAge;
+account.calcAge = 2025;
+
+account.check = 'mahmoud f';
+console.log(account.theFullName);
