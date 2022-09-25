40ad54e62a189a4a22697f49897173a69081769f
Mahmoud Fouad Ali Abo Ali <84210982+mahmoudfouadweb@users.noreply.github.com>
2022-09-25 16:40:59 +0200

    show form on click in map

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,49 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(position);
+      console.log(latitude, longitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const map = L.map('map').setView([latitude, longitude], 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      // Handling clicks on map
+      map.on('click', function (mapEvent) {
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
+    },
+    function () {
+      console.log(`application couldn't get your location`);
+    }
+  );
+
+form.addEventListener('submit', function () {
+  // Display marker
+  const { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        miWidth: 100,
+        maxWidth: 250,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('workout')
+    .openPopup();
+});
