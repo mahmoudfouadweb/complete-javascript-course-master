33c7a0b4075ae31f4e3d492fc208a2e819ba0eae
Mahmoud Fouad Ali Abo Ali <84210982+mahmoudfouadweb@users.noreply.github.com>
2022-10-15 23:34:14 +0200

    call and bind methods

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+const egyptFly = {
+  airline: 'egypt fly',
+  iataCode: 'EH',
+  bookings: [],
+};
+
+const egyptFlayBook = lufthansa.book.call(egyptFly, 33, 'Mahmoud fouad');
+console.log(lufthansa);
+const egyptBBook = lufthansa.book.bind(egyptFly);
+egyptBBook(31, 'dev mahmoud');
+
+console.log(egyptFly);
+lufthansa.plans = 300;
+lufthansa.buy = function clicking() {
+  this.plans++;
+  console.log(this.plans);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buy.bind(lufthansa));
